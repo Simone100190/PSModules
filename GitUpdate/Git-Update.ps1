35c01cefe34c21ef58c4fc0032150a7e18741103
Simone Lodi <simone.lodi8@unibo.it>
2021-02-16 15:22:23 +0100

    New Version: added help, changed variables name for being called with -x syntax

--- GitUpdate/Git-Update.ps1
+++ GitUpdate/Git-Update.ps1
@@ -1,18 +1,29 @@
 function Git-Update{
     param(
-        $repname = "status",
-        $file2add,
-        $gitcomment,
-        $wannaPush=$false
+        [string] $r = "wantToDisplayStatus", # repository name
+        $f, # file to be added
+        [string] $c, # git comment 
+        [bool] $p = $false # push yes or no
         )
-    if ($repname -ne "status")
+    if ($r -ne "wantToDisplayStatus")
     {
-    git add $file2add
-    git commit -m $gitcomment
-        if ($wannaPush)
+        git add $f
+        git commit -m $c
+        if ($p)
         {
-            git push $repname master
+            git push $r master
         }
     }
     git status
+    # new line : `r`n for ` use Alt+96
+    Write-Host "Funtion parameters: `r`n-r for repo name`r`n-f for file to be added`r`n-c for the comment`r`n-p true/false for pushing the files."
 }
+
+#     param(
+#         $r=0,
+#         $f=0,
+#         $c=0,
+#         $p=0
+#         )
+#     Write-Output "r-$r . f-$f . c-$c . p-$p"
+# }

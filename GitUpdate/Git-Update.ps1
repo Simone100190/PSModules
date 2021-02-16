0d810f358d12ad3ee5b27645ba4945f169eebc1c
Simone Lodi <simone.lodi8@unibo.it>
2021-02-16 15:38:07 +0100

    Added push without specifying reponame and added two newline in help

--- GitUpdate/Git-Update.ps1
+++ GitUpdate/Git-Update.ps1
@@ -1,22 +1,27 @@
 function Git-Update{
     param(
-        [string] $r = "wantToDisplayStatus", # repository name
-        $f, # file to be added
+        [string] $r = "THIS", # repository name
+        $f = "STATUS", # file to be added
         [string] $c, # git comment 
         [bool] $p = $false # push yes or no
         )
-    if ($r -ne "wantToDisplayStatus")
+    if ($f -ne "STATUS")
     {
         git add $f
         git commit -m $c
         if ($p)
         {
-            git push $r master
+            if ($r -eq "THIS")
+            {
+                git push
+            }else {
+                git push $r master
+            }
         }
     }
     git status
     # new line : `r`n for ` use Alt+96
-    Write-Host "Funtion parameters: `r`n-r for repo name`r`n-f for file to be added`r`n-c for the comment`r`n-p true/false for pushing the files."
+    Write-Host "`r`nFuntion parameters: `r`n-r for repo name`r`n-f for file to be added`r`n-c for the comment`r`n-p true/false for pushing the files.`r`n"
 }
 
 #     param(
